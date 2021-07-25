71067580da81f88cf00f18144ffaff2814566fb0
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-25 23:02:11 +0430

    Add Services

---
+++ src/Services/API.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Mohammadv184\ArvanCloud\Services;
+
+
+use Mohammadv184\ArvanCloud\Adapter\Adapter;
+
+interface API
+{
+    public function __construct(Adapter $http , array $config);
+}
