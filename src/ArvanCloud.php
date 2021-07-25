a170632a47eec13038f2faa31165e74d066cde64
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-25 23:02:00 +0430

    Add ArvanCloud

---
+++ src/ArvanCloud.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mohammadv184\ArvanCloud;
+
+use Mohammadv184\ArvanCloud\Adapter\Http;
+use Mohammadv184\ArvanCloud\Auth\ApiKey;
+use Mohammadv184\ArvanCloud\Auth\UserToken;
+use Mohammadv184\ArvanCloud\Exception\InvalidArgument;
+
+class ArvanCloud
+{
+    protected $config;
+    public function __construct(array $config = null)
+    {
+        $this->config=$config;
+    }
+
+    /**
+     * @throws InvalidArgument
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        $config = (new ArvanCloud)->loadConfig();
+        if (!isset($config['map'][strtolower($name)])){
+            throw new InvalidArgument("invalid Service {$name}");
+        }
+        $auth = $config['auth']['default']=='ApiKey'
+            ?new ApiKey($config['auth']['ApiKey'])
+            :new UserToken();
+        $http= new Http($auth,$config['services'][strtolower($name)]['baseUrl']);
+        return new $config['map'][strtolower($name)]($http,$config['services'][strtolower($name)],...$arguments);
+    }
+    protected function loadConfig():array
+    {
+        return require('config/arvancloud.php');
+    }
+
+}
