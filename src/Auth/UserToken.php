9aae48a0798bc46ae5b7832c74387c8a3561194b
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-25 23:00:43 +0430

    Add UserToken Auth

---
+++ src/Auth/UserToken.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Mohammadv184\ArvanCloud\Auth;
+
+
+class UserToken implements Auth
+{
+
+    public function getHeaders(): array
+    {
+        // TODO: Implement getHeaders() method.
+    }
+}
