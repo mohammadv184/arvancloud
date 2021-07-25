89196d14da876d393966c04ef007e01b762f8d44
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-25 23:02:22 +0430

    Add Cdn Service

---
+++ src/Services/Cdn/Cdn.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Mohammadv184\ArvanCloud\Services\Cdn;
+
+
+use Mohammadv184\ArvanCloud\Adapter\Adapter;
+use Mohammadv184\ArvanCloud\Exception\InvalidArgument;
+use Mohammadv184\ArvanCloud\Services\API;
+
+class Cdn implements API
+{
+    protected $http;
+
+    protected $config;
+
+    public function __construct(Adapter $http , array $config)
+    {
+        $this->http = $http;
+        $this->config = $config;
+    }
+
+    /**
+     * @throws InvalidArgument
+     */
+    public function __call($name, $arguments)
+    {
+        $endpoints= $this->config['endpoints'];
+        if (!key_exists(strtolower($name),$endpoints)){
+            throw new InvalidArgument("{$name} endpoint is invalid");
+        }
+        $domain = $this->config['domain'];
+        return new $endpoints[strtolower($name)]($this->http , $domain,...$arguments);
+    }
+}
