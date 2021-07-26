18f2e50693f53b351ba4eca12be77bc3cf2b1157
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 00:31:59 +0430

    Add HasData Trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,6 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 class TestCase extends BaseTestCase
 {
     public function test(){
-        var_dump(ArvanCloud::cdn()->domain()->get());
+        var_dump(ArvanCloud::cdn()->dns()->cloud('0a7c52a2-582e-437c-8e13-478565973f6b'));
     }
 }
