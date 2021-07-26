de310bad4ee502177f0e9ad30e26ff9c34b3520e
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 00:28:36 +0430

    fix bug

--- src/ArvanCloud.php
+++ src/ArvanCloud.php
@@ -21,13 +21,16 @@ class ArvanCloud
     public static function __callStatic($name, $arguments)
     {
         $config = (new ArvanCloud)->loadConfig();
+
+
         if (!isset($config['map'][strtolower($name)])){
             throw new InvalidArgument("invalid Service {$name}");
         }
+        $service = $config['map'][strtolower($name)];
         $auth = $config['auth']['default']=='ApiKey'
             ?new ApiKey($config['auth']['ApiKey'])
             :new UserToken();
-        $http= new Http($auth,$config['services'][strtolower($name)]['baseUrl']);
+        $http= new Http($auth,$config['services'][strtolower($name)]['baseUrl'],$service);
         return new $config['map'][strtolower($name)]($http,$config['services'][strtolower($name)],...$arguments);
     }
     protected function loadConfig():array
