a91b50ce8f9bd3787e02383c5c8b8a26f85c720e
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 00:31:21 +0430

    Update And Code Refactoring

--- src/Services/Cdn/Endpoints/Domain.php
+++ src/Services/Cdn/Endpoints/Domain.php
@@ -5,35 +5,27 @@ namespace Mohammadv184\ArvanCloud\Services\Cdn\Endpoints;
 
 
 use Mohammadv184\ArvanCloud\Adapter\Adapter;
+use Mohammadv184\ArvanCloud\Services\Cdn\Endpoint;
 use phpDocumentor\Reflection\PseudoTypes\True_;
 
-class Domain
+class Domain extends Endpoint
 {
-    protected $http;
 
-    protected $domain;
-
-    public function __construct(Adapter $http,string $defaultDomain , string $domain = null)
-    {
-        $this->http = $http;
-
-        $this->domain = $domain??$defaultDomain;
-    }
     public function all(){
-        return json_decode($this->http->get('domains')->getBody()->getContents(),true)['data'];
+        return $this->http->get('domains');
     }
     public function create($domain){
         return $this->http->post('domains/dns-service',[
            'domain' =>  $domain
-        ])->getBody()->getContents();
+        ]);
     }
     public function get($domain=null){
         $url = 'domains/'.($domain ?? $this->domain);
-        return json_decode($this->http->get($url)->getBody()->getContents(),true)['data'];
+        return $this->http->get($url);
     }
     public function delete($domain = null){
         $id = $this->get($domain ?? $this->domain)['id'];
         $url = 'domains/'.($domain ?? $this->domain)."?id=$id";
-        return json_decode($this->http->delete($url)->getBody()->getContents(),true);
+        return $this->http->delete($url);
     }
 }
