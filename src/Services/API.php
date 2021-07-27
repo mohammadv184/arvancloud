289e8637908c1a3e6c060e37efe3c9b1d29b097c
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:46:59 +0430

    Add phpdoc

--- src/Services/API.php
+++ src/Services/API.php
@@ -6,5 +6,10 @@ use Mohammadv184\ArvanCloud\Adapter\Adapter;
 
 interface API
 {
+    /**
+     * API constructor.
+     * @param Adapter $http
+     * @param array $config
+     */
     public function __construct(Adapter $http, array $config);
 }
