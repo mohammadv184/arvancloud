b7f848b4872c17f370b49871245db3fb18765b08
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:48:13 +0430

    Refactor And Add phpdoc

--- src/Services/Cdn/Endpoints/Dns.php
+++ src/Services/Cdn/Endpoints/Dns.php
@@ -2,46 +2,84 @@
 
 namespace Mohammadv184\ArvanCloud\Services\Cdn\Endpoints;
 
+use Mohammadv184\ArvanCloud\Response;
 use Mohammadv184\ArvanCloud\Services\Cdn\Endpoint;
 
 class Dns extends Endpoint
 {
-    public function all(string $domain = null)
+    /**
+     * Get All Domain Dns
+     * @param string|null $domain
+     * @return Response
+     */
+    public function all(string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/dns-records';
 
         return $this->http->get($url);
     }
 
-    public function create(array $data, string $domain = null)
+    /**
+     * Create new Domain Dns
+     * @param array $data
+     * @param string|null $domain
+     * @return Response
+     */
+    public function create(array $data, string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/dns-records';
 
         return $this->http->post($url, $data);
     }
 
-    public function get($id, string $domain = null)
+    /**
+     * Get Domain Dns Settings
+     * @param string $id
+     * @param string|null $domain
+     * @return Response
+     */
+    public function get(string $id, string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/dns-records/'.$id;
 
         return $this->http->get($url);
     }
 
-    public function update($id, array $data, string $domain = null)
+    /**
+     * Update Domain Dns Settings
+     * @param string $id
+     * @param array $data
+     * @param string|null $domain
+     * @return Response
+     */
+    public function update(string $id, array $data, string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/dns-records/'.$id;
 
         return $this->http->put($url, $data);
     }
 
-    public function delete($id, string $domain = null)
+    /**
+     * Delete Domain Dns
+     * @param string $id
+     * @param string|null $domain
+     * @return Response
+     */
+    public function delete(string $id, string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/dns-records/'.$id;
 
         return $this->http->delete($url);
     }
 
-    public function cloud($id, bool $status = true, string $domain = null)
+    /**
+     * Update Domain Dns Cloud Status
+     * @param string $id
+     * @param bool $status
+     * @param string|null $domain
+     * @return Response
+     */
+    public function cloud(string $id, bool $status = true, string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/dns-records/'.$id.'/cloud';
 
