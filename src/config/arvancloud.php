10203976babc496614d9f3d7271ff49b1e04b8c1
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:46:37 +0430

    Add phpdoc configs

--- src/config/arvancloud.php
+++ src/config/arvancloud.php
@@ -1,12 +1,30 @@
 <?php
 
 return[
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Config
+    |--------------------------------------------------------------------------
+    |
+    | These are the list of Authentication Config.
+    |
+    */
     'auth'=> [
         'default'  => 'ApiKey',
         'UserToken'=> '',
         'ApiKey'   => '',
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | List of Services
+    |--------------------------------------------------------------------------
+    |
+    | These are the list of services to use for this package.
+    | You can change the name. Then you'll have to change
+    | it in the map array too.
+    |
+    */
     'services'=> [
         'cdn'=> [
             'baseUrl'  => 'https://napi.arvancloud.com/cdn/4.0/',
@@ -20,6 +38,18 @@ return[
         ],
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Class Maps
+    |--------------------------------------------------------------------------
+    |
+    | This is the array of Classes that maps to Services above.
+    | You can create your own Service if you like and add the
+    | config in the Services array and the class to use for
+    | here with the same name. You will have to extend
+    | Mohammadv184\ArvanCloud\Services\API in your Service.
+    |
+    */
     'map'=> [
         'cdn'=> \Mohammadv184\ArvanCloud\Services\Cdn\Cdn::class,
     ],
