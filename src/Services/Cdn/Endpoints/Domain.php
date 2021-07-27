a7d354b052b835ac627a141b8ce8e7ee180914ac
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:48:22 +0430

    Add phpdoc

--- src/Services/Cdn/Endpoints/Domain.php
+++ src/Services/Cdn/Endpoints/Domain.php
@@ -2,30 +2,50 @@
 
 namespace Mohammadv184\ArvanCloud\Services\Cdn\Endpoints;
 
+use Mohammadv184\ArvanCloud\Response;
 use Mohammadv184\ArvanCloud\Services\Cdn\Endpoint;
 
 class Domain extends Endpoint
 {
-    public function all()
+    /**
+     * Get All User Domains
+     * @return Response
+     */
+    public function all():Response
     {
         return $this->http->get('domains');
     }
 
-    public function create($domain)
+    /**
+     * Create New User Domain
+     * @param string $domain
+     * @return Response
+     */
+    public function create(string $domain):Response
     {
         return $this->http->post('domains/dns-service', [
             'domain' => $domain,
         ]);
     }
 
-    public function get($domain = null)
+    /**
+     * Get User Domain
+     * @param string|null $domain
+     * @return Response
+     */
+    public function get(string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain);
 
         return $this->http->get($url);
     }
 
-    public function delete($domain = null)
+    /**
+     * Delete User Domain
+     * @param string|null $domain
+     * @return Response
+     */
+    public function delete(string $domain = null):Response
     {
         $id = $this->get($domain ?? $this->domain)['id'];
         $url = 'domains/'.($domain ?? $this->domain)."?id=$id";
