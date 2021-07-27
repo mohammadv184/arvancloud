311eb51c2c25b15ba09f4d2f1e1d5fc32c2a9292
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:46:47 +0430

    Refactor Response And Add phpdoc

--- src/Response.php
+++ src/Response.php
@@ -9,31 +9,57 @@ class Response extends ResponseAbstract implements \ArrayAccess
 {
     use HasData;
 
+    /**
+     * set Response Data
+     * @param $name
+     * @param $value
+     */
     public function __set($name, $value)
     {
         $this->data($name, $value);
     }
 
+    /**
+     * get Response Data
+     * @param $name
+     * @return mixed|null
+     */
     public function __get($name)
     {
         return $this->GetData($name);
     }
 
+    /**
+     * @param mixed $offset
+     * @return bool
+     */
     public function offsetExists($offset): bool
     {
         return isset($this->data[$offset]);
     }
 
+    /**
+     * @param mixed $offset
+     * @return mixed
+     */
     public function offsetGet($offset)
     {
         return $this->data[$offset];
     }
 
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     * @return mixed|void
+     */
     public function offsetSet($offset, $value)
     {
         return $this->data[$offset] = $value;
     }
 
+    /**
+     * @param mixed $offset
+     */
     public function offsetUnset($offset)
     {
         unset($this->data[$offset]);
