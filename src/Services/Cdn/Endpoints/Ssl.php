4d2ec8aaab8559124f896888b13ecb7b0f325325
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:48:38 +0430

    Refactor Ssl And Add phpdoc

--- src/Services/Cdn/Endpoints/Ssl.php
+++ src/Services/Cdn/Endpoints/Ssl.php
@@ -2,18 +2,30 @@
 
 namespace Mohammadv184\ArvanCloud\Services\Cdn\Endpoints;
 
+use Mohammadv184\ArvanCloud\Response;
 use Mohammadv184\ArvanCloud\Services\Cdn\Endpoint;
 
 class Ssl extends Endpoint
 {
-    public function get(string $domain = null)
+    /**
+     * Get Domain Ssl Settings
+     * @param string|null $domain
+     * @return Response
+     */
+    public function get(string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/ssl';
 
         return $this->http->get($url);
     }
 
-    public function update(string $sslType, string $domain = null)
+    /**
+     * Update Domain Ssl Settings
+     * @param string $sslType
+     * @param string|null $domain
+     * @return Response
+     */
+    public function update(string $sslType, string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/ssl';
         $data = [
