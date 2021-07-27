61d43903ee2bf5a41fd678fee5f0d815c4549808
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 12:57:01 +0430

    update tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,8 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 class TestCase extends BaseTestCase
 {
     public function test(){
-        var_dump(ArvanCloud::cdn()->dns()->cloud('0a7c52a2-582e-437c-8e13-478565973f6b'));
+        var_dump(ArvanCloud::cdn()->cache()->purge([
+            'https://di-gi-mall.ir'
+        ]));
     }
 }
