b2394d44b6eca007747b759f93c058b29d146de5
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:45:45 +0430

    Refactor And add phpdoc

--- src/Auth/ApiKey.php
+++ src/Auth/ApiKey.php
@@ -4,13 +4,25 @@ namespace Mohammadv184\ArvanCloud\Auth;
 
 class ApiKey implements Auth
 {
+    /**
+     * ArvanCloud User apikey
+     * @var string
+     */
     protected $apiKey;
 
-    public function __construct($apiKey)
+    /**
+     * ApiKey constructor.
+     * @param string $apiKey
+     */
+    public function __construct(string $apiKey)
     {
         $this->apiKey = $apiKey;
     }
 
+    /**
+     * get Request Headers
+     * @return string[]
+     */
     public function getHeaders(): array
     {
         return [
