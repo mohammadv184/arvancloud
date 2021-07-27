873b11d11bd42c84869e92becf8ddae063bc2ae8
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 15:12:00 +0430

    Update config

--- src/config/arvancloud.php
+++ src/config/arvancloud.php
@@ -4,13 +4,13 @@ return[
     'auth'=>[
         'default' =>'ApiKey',
         'UserToken'=>'',
-        'ApiKey'=>'Apikey 2187d7f6-ecec-4309-8e35-37c3b0c92e69',
+        'ApiKey'=>'',
     ],
 
     'services'=>[
         'cdn'=>[
             'baseUrl'=>'https://napi.arvancloud.com/cdn/4.0/',
-            'domain' => 'di-gi-mall.ir',
+            'domain' => 'your_domain.com',
             'endpoints'=>[
                 'domain'=>\Mohammadv184\ArvanCloud\Services\Cdn\Endpoints\Domain::class,
                 'dns'=>\Mohammadv184\ArvanCloud\Services\Cdn\Endpoints\Dns::class,
