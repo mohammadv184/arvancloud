e57afeb81b666c130f612229e54870c1b285f1a2
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:48:06 +0430

    Refactor Cache  And Add phpdoc

--- src/Services/Cdn/Endpoints/Cache.php
+++ src/Services/Cdn/Endpoints/Cache.php
@@ -2,25 +2,43 @@
 
 namespace Mohammadv184\ArvanCloud\Services\Cdn\Endpoints;
 
+use Mohammadv184\ArvanCloud\Response;
 use Mohammadv184\ArvanCloud\Services\Cdn\Endpoint;
 
 class Cache extends Endpoint
 {
-    public function get(string $domain = null)
+    /**
+     * Get Domain Cache settings
+     * @param string|null $domain
+     * @return \Mohammadv184\ArvanCloud\Response
+     */
+    public function get(string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/caching';
 
         return $this->http->get($url);
     }
 
-    public function update(array $data, string $domain = null)
+    /**
+     * Update Domain Cache settings
+     * @param array $data
+     * @param string|null $domain
+     * @return Response
+     */
+    public function update(array $data, string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/caching';
 
         return $this->http->patch($url, $data);
     }
 
-    public function purge(array $urls = null, string $domain = null)
+    /**
+     * Purge Domain Cache
+     * @param array|null $urls
+     * @param string|null $domain
+     * @return Response
+     */
+    public function purge(array $urls = null, string $domain = null):Response
     {
         $url = 'domains/'.($domain ?? $this->domain).'/caching';
         $data = [
