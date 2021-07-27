46cb298b5478daa2964d64eede423bc6352c6a2a
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:44:47 +0430

    Refactor and Add phpdoc

--- src/Abstracts/ResponseInterface.php
+++ src/Abstracts/ResponseInterface.php
@@ -7,14 +7,20 @@ use Carbon\Carbon;
 interface ResponseInterface
 {
     /**
+     * Get Response ArvanCloud Service
      * @return string
      */
     public function getService(): string;
 
     /**
+     * Get Response Date
      * @return Carbon
      */
     public function getDate(): Carbon;
 
+    /**
+     * Get Response Message
+     * @return string
+     */
     public function getMessage(): string;
 }
