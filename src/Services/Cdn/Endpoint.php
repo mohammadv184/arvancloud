d0b726fd9b8c5a23c52eadff2f266b9d86e3e622
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-27 16:47:40 +0430

    Add Endpoint phpdoc

--- src/Services/Cdn/Endpoint.php
+++ src/Services/Cdn/Endpoint.php
@@ -6,10 +6,24 @@ use Mohammadv184\ArvanCloud\Adapter\Adapter;
 
 abstract class Endpoint
 {
+    /**
+     * ArvanCloud Http Adapter
+     * @var Adapter
+     */
     protected $http;
 
+    /**
+     * User Domain
+     * @var string
+     */
     protected $domain;
 
+    /**
+     * Endpoint constructor.
+     * @param Adapter $http
+     * @param string $defaultDomain
+     * @param string|null $domain
+     */
     public function __construct(Adapter $http, string $defaultDomain, string $domain = null)
     {
         $this->http = $http;
