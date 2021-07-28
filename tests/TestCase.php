be0fcd4634c389bd4f260e0496322e3db9750f15
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-28 18:53:12 +0430

    add tests skeleton

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,26 @@
 
 namespace Mohammadv184\ArvanCloud\Tests;
 
+use Mohammadv184\ArvanCloud\Response;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
-class TestCase extends BaseTestCase
+abstract class TestCase extends BaseTestCase
 {
-    public function test()
+    /**
+     * get Fake Response
+     * @param string $path
+     * @param string $service
+     * @return Response
+     */
+    public function getResponse(string $path, string $service): Response
     {
-        $this->assertTrue(true);
+        $file = file_get_contents(__DIR__.'/FakeResponse/'.$path);
+
+        $responseData =json_decode($file,true) ;
+
+        $response = new Response($service,$responseData['message']??'');
+        $response->data($responseData['data']??$responseData);
+
+        return $response;
     }
 }
