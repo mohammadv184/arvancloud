0af40b7a0026ed35a3a50b790e87e7e6e7630506
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-28 19:04:20 +0430

    Apply fixes from StyleCI (#4)
    *Apply fixes from StyleCI

--- tests/Adapter/HttpTest.php
+++ tests/Adapter/HttpTest.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Mohammadv184\ArvanCloud\Tests\Adapter;
-
 
 use Mohammadv184\ArvanCloud\Adapter\Adapter;
 use Mohammadv184\ArvanCloud\Adapter\Http;
@@ -14,100 +12,112 @@ use Mohammadv184\ArvanCloud\Tests\TestCase;
 class HttpTest extends TestCase
 {
     protected $http;
+
     protected function setUp(): void
     {
         $auth = new ApiKey('ApiKey test1654861648jbhkjniyb');
 
-        $this->http = new Http($auth,'https://httpbin.org/',static::class);
+        $this->http = new Http($auth, 'https://httpbin.org/', static::class);
     }
-    public function testInstance(){
-        $this->assertInstanceOf(Adapter::class , $this->http);
+
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Adapter::class, $this->http);
     }
-    public function testGet(){
+
+    public function testGet()
+    {
         $data = [
             'foo' => 'bar',
 
-            'message' => 'ok!'
+            'message' => 'ok!',
         ];
 
-        $response = $this->http->get('get',$data);
+        $response = $this->http->get('get', $data);
 
-        $this->assertInstanceOf(Response::class,$response);
+        $this->assertInstanceOf(Response::class, $response);
 
-        $this->assertEquals(static::class,$response->getService());
+        $this->assertEquals(static::class, $response->getService());
 
-        $this->assertEquals($data,$response->getData()['args']);
+        $this->assertEquals($data, $response->getData()['args']);
 
-        $this->assertArrayHasKey('Authorization',$response->getData()['headers']);
+        $this->assertArrayHasKey('Authorization', $response->getData()['headers']);
 
-        $this->assertEquals('ApiKey test1654861648jbhkjniyb',$response['headers']['Authorization']);
+        $this->assertEquals('ApiKey test1654861648jbhkjniyb', $response['headers']['Authorization']);
     }
-    public function testPost(){
+
+    public function testPost()
+    {
         $data = [
             'foo' => 'bar',
 
-            'message' => 'ok!'
+            'message' => 'ok!',
         ];
 
-        $response = $this->http->post('post',$data);
+        $response = $this->http->post('post', $data);
 
-        $this->assertInstanceOf(Response::class,$response);
+        $this->assertInstanceOf(Response::class, $response);
 
-        $this->assertEquals(static::class,$response->getService());
+        $this->assertEquals(static::class, $response->getService());
 
-        $this->assertEquals($data,$response->getData()['json']);
+        $this->assertEquals($data, $response->getData()['json']);
 
-        $this->assertArrayHasKey('Authorization',$response->getData()['headers']);
+        $this->assertArrayHasKey('Authorization', $response->getData()['headers']);
 
-        $this->assertEquals('ApiKey test1654861648jbhkjniyb',$response['headers']['Authorization']);
-
+        $this->assertEquals('ApiKey test1654861648jbhkjniyb', $response['headers']['Authorization']);
     }
 
-
-    public function testPatch(){
+    public function testPatch()
+    {
         $data = [
             'foo' => 'bar',
 
-            'message' => 'ok!'
+            'message' => 'ok!',
         ];
 
-        $response = $this->http->patch('patch',$data);
+        $response = $this->http->patch('patch', $data);
 
-        $this->assertInstanceOf(Response::class,$response);
+        $this->assertInstanceOf(Response::class, $response);
 
-        $this->assertEquals(static::class,$response->getService());
+        $this->assertEquals(static::class, $response->getService());
 
-        $this->assertEquals($data,$response->getData()['json']);
+        $this->assertEquals($data, $response->getData()['json']);
 
-        $this->assertArrayHasKey('Authorization',$response->getData()['headers']);
+        $this->assertArrayHasKey('Authorization', $response->getData()['headers']);
 
-        $this->assertEquals('ApiKey test1654861648jbhkjniyb',$response['headers']['Authorization']);
+        $this->assertEquals('ApiKey test1654861648jbhkjniyb', $response['headers']['Authorization']);
     }
-    public function testDelete(){
+
+    public function testDelete()
+    {
         $data = [
             'foo' => 'bar',
 
-            'message' => 'ok!'
+            'message' => 'ok!',
         ];
 
-        $response = $this->http->delete('delete',$data);
+        $response = $this->http->delete('delete', $data);
 
-        $this->assertInstanceOf(Response::class,$response);
+        $this->assertInstanceOf(Response::class, $response);
 
-        $this->assertEquals(static::class,$response->getService());
+        $this->assertEquals(static::class, $response->getService());
 
-        $this->assertEquals($data,$response->getData()['json']);
+        $this->assertEquals($data, $response->getData()['json']);
 
-        $this->assertArrayHasKey('Authorization',$response->getData()['headers']);
+        $this->assertArrayHasKey('Authorization', $response->getData()['headers']);
 
-        $this->assertEquals('ApiKey test1654861648jbhkjniyb',$response['headers']['Authorization']);
+        $this->assertEquals('ApiKey test1654861648jbhkjniyb', $response['headers']['Authorization']);
     }
-    public function testError404(){
-       $this->expectException(ResponseException::class);
 
-       $this->http->get('status/404');
+    public function testError404()
+    {
+        $this->expectException(ResponseException::class);
+
+        $this->http->get('status/404');
     }
-    public function testServerError(){
+
+    public function testServerError()
+    {
         $this->expectException(ResponseException::class);
 
         $this->http->get('status/500');
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,19 +8,21 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     /**
-     * get Fake Response
+     * get Fake Response.
+     *
      * @param string $path
      * @param string $service
+     *
      * @return Response
      */
     public function getResponse(string $path, string $service): Response
     {
         $file = file_get_contents(__DIR__.'/FakeResponse/'.$path);
 
-        $responseData =json_decode($file,true) ;
+        $responseData = json_decode($file, true);
 
-        $response = new Response($service,$responseData['message']??'');
-        $response->data($responseData['data']??$responseData);
+        $response = new Response($service, $responseData['message'] ?? '');
+        $response->data($responseData['data'] ?? $responseData);
 
         return $response;
     }
